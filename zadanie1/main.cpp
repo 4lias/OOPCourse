60d4e237af0f7731efd0c0bef94b219f02fe3b2b
4lias <52102271+4lias@users.noreply.github.com>
2024-03-16 23:35:58 +0100

    w pełni działający program

--- zadanie1/main.cpp
+++ zadanie1/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string x;
+    //int y = 0;
+
+    cout << "x = ";
+    cin >> x;
+    int y = stoi(x);
+    do
+    {
+        cout << "press e to exit or y = ";
+        cin >> x;
+
+        if(x!="e")
+        {
+            y = y*stoi(x);
+            cout << "x = " << y << endl; 
+        }
+    } while (x!="e");
+    cout << "Exiting" << endl;
+
     return 0;
 }
